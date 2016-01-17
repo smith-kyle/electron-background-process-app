38da5e4de9c9bfe7651b67d0d0509e2509ac09f4
Kyle Smith <kyles@axosoft.com>
2016-01-17 15:27:05 -0700

    Made CPU intensive function more CPU intensive

--- cpuIntensive.js
+++ cpuIntensive.js
@@ -1,6 +1,10 @@
 const cpuIntensive = {
-  doStuff(a, b) {
-    return a + b;
+  doStuff() {
+    var value = 0;
+    for (var i = 0; i < 100000000; i++) {
+      value += Math.random();
+    }
+    return Math.floor(value / Math.random());
   }
 };
 
