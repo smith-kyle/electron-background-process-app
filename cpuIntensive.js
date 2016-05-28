49853218db02ed033c88360bec32c0c04adf9366
Kyle Smith <kyles@axosoft.com>
2016-05-28 13:33:02 -0700

    Added callback example

--- cpuIntensive.js
+++ cpuIntensive.js
@@ -5,6 +5,15 @@ const cpuIntensive = {
       value += Math.random();
     }
     return Math.floor(value / Math.random());
+  },
+
+  doCallbackStuff(cb) {
+    var newPercent = 0;
+    for (var i = 0; i < 100000000; i++) {
+      if (newPercent === Math.floor(i / 100000000 * 100)) {
+        cb(Math.floor(newPercent++));
+      }
+    }
   }
 };
 
